06636e06a79fbaf50dc6f9c31b9ffec6e2e3b92b
saiganesh marati <maratisaiganesh123@gmail.com>
2024-11-02 22:05:37 +0530

    recapctha validations

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-function beforesubmit()
-{
-    let outputdate=document.querySelector(".outputdate");
-    let inputdate=document.querySelector(".inputdate");
-    console.log("inputdate.value",inputdate.value);// string -->date (en_IN)
+let capthachecked=false;
 
-    let formattedDate= new Date(inputdate.value).toLocaleDateString("en-IN");
-    console.log("formattedDate",formattedDate);
-    outputdate.value=formattedDate;
+
+function beforesubmit(event)
+{
+    if(capthachecked){
+        let outputdate=document.querySelector(".outputdate");
+        let inputdate=document.querySelector(".inputdate");
+        console.log("inputdate.value",inputdate.value);// string -->date (en_IN)
+    
+        let formattedDate= new Date(inputdate.value).toLocaleDateString("en-IN");
+        console.log("formattedDate",formattedDate);
+        outputdate.value=formattedDate;
+    }
+    else{
+        alert("Please Check the reCAPTCHA box to submit the lead");
+        event.preventDefault();
+    }
+    
 }
 
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+function capthasuccess()
+{
+    capthachecked=true;
+}
